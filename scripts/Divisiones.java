a9e2cb3ecba2c3a458531f8d03255df1a024c4d8
Toni <antonimanuel2094@gmail.com>
2022-03-26 20:02:05 +0100

    Scripts con operaciones matematicas

---
+++ scripts/Divisiones.java
@@ -0,0 +1,14 @@
+
+
+public class Divisiones{
+
+    public long divisionEnteros(int num1, int num2){
+        long res;
+
+        res = num1 % num2;
+
+        return res;
+    }
+    
+
+}
---
+++ scripts/Multiplicaciones.java
@@ -0,0 +1,12 @@
+public class Multiplicaciones{
+
+    public long multiplicacion(int num1, int num2){
+        long res;
+
+        res = num1 * num2;
+
+        return res;
+    }
+    
+
+}
