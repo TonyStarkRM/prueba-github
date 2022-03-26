3720d40fe232bf91fc0b7e2687b8a538184d1c3a
Toni <antonimanuel2094@gmail.com>
2022-03-26 19:32:10 +0100

    Se modifica el fichero multiplicaciones de forma erronea

--- Multiplicaciones.java
+++ Multiplicaciones.java
@@ -3,7 +3,7 @@ public class Multiplicaciones{
     public long multiplicacion(int num1, int num2){
         long res;
 
-        res = num1 * num2;
+        res = num1 + num2;
 
         return res;
     }
