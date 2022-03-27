fe0ddf8317f6577b65e142b09441f63852102db1
Toni <antonimanuel2094@gmail.com>
2022-03-27 11:55:37 +0200

    Modificado el archivo de stash

--- pruebecita.java
+++ pruebecita.java
@@ -2,6 +2,8 @@ public class pruebecita{
 
     public static void main(String[] args) {
         
-        System.out.println("Cambios conflictivos.");
+        System.out.println("Cambios en este fichero. Se hizo Stash y funcionó. Solucion 2.");
     }
+
 }
+
