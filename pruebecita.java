78415385019d3367b58aebe247d0eef517d5679a
Toni <antonimanuel2094@gmail.com>
2022-03-27 12:20:49 +0200

    Cambios realziados sobre pruebecita

--- pruebecita.java
+++ pruebecita.java
@@ -3,6 +3,9 @@ public class pruebecita{
     public static void main(String[] args) {
         
         System.out.println("Cambios en este fichero. Se hizo Stash y funcionó. Solucion 2.");
+    
+	System.out.println("cambios");
+
     }
 
 }
