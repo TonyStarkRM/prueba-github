cfec9b3dbfa9f10f0c16bd07af8843bc304202e2
Toni <antonimanuel2094@gmail.com>
2022-03-27 21:11:01 +0200

    Squashed commit of the following:
    commit 51f2bc9b28c178d1d98008309b815f3094a660a7
    Author: Toni <antonimanuel2094@gmail.com>
    Date:   Sun Mar 27 21:10:58 2022 +0200
    
    	Merge branch 'feature/feature-paquete-clasesmaths'

--- src/prueba/Principal.java
+++ src/prueba/Principal.java
@@ -1,11 +1,24 @@
 package prueba;
 
+import maths.Division;
+import maths.Multiplicacion;
+import maths.Resta;
+import maths.Suma;
+
 public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		System.out.println("Hola que pasa");
+		
+		int num1 = 5;
+		int num2 = 3;
+		
+		System.out.println("Suma de " + num1 + " y " + num2 + ": " + Suma.suma(num1, num2));
+		System.out.println("Resta de " + num1 + " y " + num2 + ": " + Resta.resta(num1, num2));
+		System.out.println("Multiplicacion de " + num1 + " y " + num2 + ": " + Multiplicacion.multiplicacion(num1, num2));
+		System.out.println("Division de " + num1 + " y " + num2 + ": " + Division.division(num1, num2));
 
 	}
 
