d8421fcb91d27adbc21b56953e7c6c5619d4d260
Antonio Manuel <102467922+TonyStarkRM@users.noreply.github.com>
2022-03-27 21:08:08 +0200

    Modificamos la clase principal desde gitHub

--- src/prueba/Principal.java
+++ src/prueba/Principal.java
@@ -5,7 +5,9 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hola que pasa");
+		System.out.println("Hola que pasa, vamos a añadir algo");
+		
+		System.out.println("Como quieras");
 
 	}
 
