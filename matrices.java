a923f3cb1df470dd25162232d3f0c23f3fa59de3
Toni <antonimanuel2094@gmail.com>
2022-03-27 12:23:27 +0200

    Cambios sobre matrices

--- matrices.java
+++ matrices.java
@@ -3,6 +3,6 @@ public class matrices{
 
     public static void main(String[] args) {
         
-        System.out.println("Script para hacer matrices.");
+        System.out.println("Script para hacer matrices 1.");
     }
 }
